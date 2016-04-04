5dc2c333293913ad7ad5f1f64b2200845624bfb9
Velibor Pjevalica <velibor.pjevalica@gmail.com>
2016-04-04 10:54:23 +0200

    First upload

---
+++ app/src/main/java/cirvirlab/interfragmentcommunication/Lower.java
@@ -0,0 +1,30 @@
+package cirvirlab.interfragmentcommunication;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Lower extends Fragment {
+
+
+    public Lower() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_lower, container, false);
+        return view;
+    }
+
+
+
+}
